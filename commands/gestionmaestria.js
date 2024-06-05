0d8c253bc31bafec8f5f2f544e40205cfa9f2e1c
Lord Eiser’s brick’s <168234265+XU3legacy@users.noreply.github.com>
2024-06-05 20:50:32 +0100

    Update gestionmaestria.js

--- commands/gestionmaestria.js
+++ commands/gestionmaestria.js
@@ -460,3 +460,551 @@ france(
         }
     }
 );
+
+
+france(
+    {
+        nomCom: 'Keikaiser.stat',
+        categorie: 'Maestria rpverse'
+    },
+    async (dest, zk, commandeOptions) => {
+        const { repondre, arg, ms } = commandeOptions;
+
+        if (!arg || arg.length === 0)  {
+            const lien = 'https://telegra.ph/file/9a1c19808b8b4e6bcf636.jpg';
+            const msg = `╭━━━「 𝚳𝚫𝚵𝐒𝚻𝚪𝚰𝚫 」❏
+┆ *_♯ROLE PLAYVERSE_*
+╰━━━━━━━━━━━━━❖
+*❖Pseudo*👤:「 KEIKAISER」
+*❖Type de mage*👤:「 *_MAHOTSUKAI_*」
+*❖Race*:「 *_HUMAIN_*」
+*❖Rang*:「E 」
+*❖Classe*:「 *_NOVICE 🥉_*」
+        ━━━━━━━━━━━━╮
+                                                           ┆
+❖━━━━━━━━━━━━━╯
+*défaites: 00*
+*Nombre de victoires: 00*
+*_Trophee : 00🏆_*
+╭━━━「 𝚳𝚫𝚵𝐒𝚻𝚪𝚰𝚫 」❏
+┆ *_♯TOTAL FIGHT SEASON: 00 ⚔️🛡️_*
+╰━━━━━━━━━━━━━❖
+*❏ Gain duel : 00 🧭*
+*❏ Gain quête 00 🧭:*
+*❏ Gain aventure : 00 🧭*
+*❏ Solde: 1000 🧭*
+
+     ━━━━━━━━━━━
+━━━━━━
+*Equipment🎒: 00🥋*`;
+            zk.sendMessage(dest, { image: { url: lien }, caption: msg }, { quoted: ms });
+   
+        }
+    }
+);
+
+
+france(
+    {
+        nomCom: 'Killua.stat',
+        categorie: 'Maestria rpverse'
+    },
+    async (dest, zk, commandeOptions) => {
+        const { repondre, arg, ms } = commandeOptions;
+
+        if (!arg || arg.length === 0)  {
+            const lien = 'https://telegra.ph/file/9a1c19808b8b4e6bcf636.jpg';
+            const msg = `╭━━━「 𝚳𝚫𝚵𝐒𝚻𝚪𝚰𝚫 」❏
+┆ *_♯ROLE PLAYVERSE_*
+╰━━━━━━━━━━━━━❖
+*❖Pseudo*👤:「 KILLUA」
+*❖Type de mage*👤:「 *_MAHOTSUKAI_*」
+*❖Race*:「 *_HUMAIN_*」
+*❖Rang*:「E 」
+*❖Classe*:「 *_NOVICE 🥉_*」
+        ━━━━━━━━━━━━╮
+                                                           ┆
+❖━━━━━━━━━━━━━╯
+*défaites: 00*
+*Nombre de victoires: 00*
+*_Trophee : 00🏆_*
+╭━━━「 𝚳𝚫𝚵𝐒𝚻𝚪𝚰𝚫 」❏
+┆ *_♯TOTAL FIGHT SEASON: 00 ⚔️🛡️_*
+╰━━━━━━━━━━━━━❖
+*❏ Gain duel : 00 🧭*
+*❏ Gain quête 00 🧭:*
+*❏ Gain aventure : 00 🧭*
+*❏ Solde: 1000 🧭*
+
+     ━━━━━━━━━━━
+━━━━━━
+*Equipment🎒: 00🥋*`;
+            zk.sendMessage(dest, { image: { url: lien }, caption: msg }, { quoted: ms });
+   
+        }
+    }
+);
+
+
+france(
+    {
+        nomCom: 'Lefoudroyant.stat',
+        categorie: 'Maestria rpverse'
+    },
+    async (dest, zk, commandeOptions) => {
+        const { repondre, arg, ms } = commandeOptions;
+
+        if (!arg || arg.length === 0)  {
+            const lien = 'https://telegra.ph/file/9a1c19808b8b4e6bcf636.jpg';
+            const msg = `╭━━━「 𝚳𝚫𝚵𝐒𝚻𝚪𝚰𝚫 」❏
+┆ *_♯ROLE PLAYVERSE_*
+╰━━━━━━━━━━━━━❖
+*❖Pseudo*👤:「 LE FOUDROYANT」
+*❖Type de mage*👤:「 *_MAHOTSUKAI_*」
+*❖Race*:「 *_HUMAIN_*」
+*❖Rang*:「E 」
+*❖Classe*:「 *_NOVICE 🥉_*」
+        ━━━━━━━━━━━━╮
+                                                           ┆
+❖━━━━━━━━━━━━━╯
+*défaites: 00*
+*Nombre de victoires: 00*
+*_Trophee : 00🏆_*
+╭━━━「 𝚳𝚫𝚵𝐒𝚻𝚪𝚰𝚫 」❏
+┆ *_♯TOTAL FIGHT SEASON: 00 ⚔️🛡️_*
+╰━━━━━━━━━━━━━❖
+*❏ Gain duel : 00 🧭*
+*❏ Gain quête 00 🧭:*
+*❏ Gain aventure : 00 🧭*
+*❏ Solde: 1000 🧭*
+
+     ━━━━━━━━━━━
+━━━━━━
+*Equipment🎒: 00🥋*`;
+            zk.sendMessage(dest, { image: { url: lien }, caption: msg }, { quoted: ms });
+   
+        }
+    }
+);
+
+
+france(
+    {
+        nomCom: 'Iambusy.stat',
+        categorie: 'Maestria rpverse'
+    },
+    async (dest, zk, commandeOptions) => {
+        const { repondre, arg, ms } = commandeOptions;
+
+        if (!arg || arg.length === 0)  {
+            const lien = 'https://telegra.ph/file/9a1c19808b8b4e6bcf636.jpg';
+            const msg = `╭━━━「 𝚳𝚫𝚵𝐒𝚻𝚪𝚰𝚫 」❏
+┆ *_♯ROLE PLAYVERSE_*
+╰━━━━━━━━━━━━━❖
+*❖Pseudo*👤:「 I AM BUSY」
+*❖Type de mage*👤:「 *_MAHOTSUKAI_*」
+*❖Race*:「 *_HUMAIN_*」
+*❖Rang*:「E 」
+*❖Classe*:「 *_NOVICE 🥉_*」
+        ━━━━━━━━━━━━╮
+                                                           ┆
+❖━━━━━━━━━━━━━╯
+*défaites: 00*
+*Nombre de victoires: 00*
+*_Trophee : 00🏆_*
+╭━━━「 𝚳𝚫𝚵𝐒𝚻𝚪𝚰𝚫 」❏
+┆ *_♯TOTAL FIGHT SEASON: 00 ⚔️🛡️_*
+╰━━━━━━━━━━━━━❖
+*❏ Gain duel : 00 🧭*
+*❏ Gain quête 00 🧭:*
+*❏ Gain aventure : 00 🧭*
+*❏ Solde: 1000 🧭*
+
+     ━━━━━━━━━━━
+━━━━━━
+*Equipment🎒: 00🥋*`;
+            zk.sendMessage(dest, { image: { url: lien }, caption: msg }, { quoted: ms });
+   
+        }
+    }
+);
+
+
+france(
+    {
+        nomCom: 'Leywin.stat',
+        categorie: 'Maestria rpverse'
+    },
+    async (dest, zk, commandeOptions) => {
+        const { repondre, arg, ms } = commandeOptions;
+
+        if (!arg || arg.length === 0)  {
+            const lien = 'https://telegra.ph/file/9a1c19808b8b4e6bcf636.jpg';
+            const msg = `╭━━━「 𝚳𝚫𝚵𝐒𝚻𝚪𝚰𝚫 」❏
+┆ *_♯ROLE PLAYVERSE_*
+╰━━━━━━━━━━━━━❖
+*❖Pseudo*👤:「 LEYWIN LAMPROGUE」
+*❖Type de mage*👤:「 *_MAHOTSUKAI_*」
+*❖Race*:「 *_HUMAIN_*」
+*❖Rang*:「E 」
+*❖Classe*:「 *_NOVICE 🥉_*」
+        ━━━━━━━━━━━━╮
+                                                           ┆
+❖━━━━━━━━━━━━━╯
+*défaites: 00*
+*Nombre de victoires: 00*
+*_Trophee : 00🏆_*
+╭━━━「 𝚳𝚫𝚵𝐒𝚻𝚪𝚰𝚫 」❏
+┆ *_♯TOTAL FIGHT SEASON: 00 ⚔️🛡️_*
+╰━━━━━━━━━━━━━❖
+*❏ Gain duel : 00 🧭*
+*❏ Gain quête 00 🧭:*
+*❏ Gain aventure : 00 🧭*
+*❏ Solde: 1000 🧭*
+
+     ━━━━━━━━━━━
+━━━━━━
+*Equipment🎒: 00🥋*`;
+            zk.sendMessage(dest, { image: { url: lien }, caption: msg }, { quoted: ms });
+   
+        }
+    }
+);
+
+
+france(
+    {
+        nomCom: 'Lust.stat',
+        categorie: 'Maestria rpverse'
+    },
+    async (dest, zk, commandeOptions) => {
+        const { repondre, arg, ms } = commandeOptions;
+
+        if (!arg || arg.length === 0)  {
+            const lien = 'https://telegra.ph/file/9a1c19808b8b4e6bcf636.jpg';
+            const msg = `╭━━━「 𝚳𝚫𝚵𝐒𝚻𝚪𝚰𝚫 」❏
+┆ *_♯ROLE PLAYVERSE_*
+╰━━━━━━━━━━━━━❖
+*❖Pseudo*👤:「 LUST VERMILLON」
+*❖Type de mage*👤:「 *_MAHOTSUKAI_*」
+*❖Race*:「 *_HUMAIN_*」
+*❖Rang*:「E 」
+*❖Classe*:「 *_NOVICE 🥉_*」
+        ━━━━━━━━━━━━╮
+                                                           ┆
+❖━━━━━━━━━━━━━╯
+*défaites: 00*
+*Nombre de victoires: 00*
+*_Trophee : 00🏆_*
+╭━━━「 𝚳𝚫𝚵𝐒𝚻𝚪𝚰𝚫 」❏
+┆ *_♯TOTAL FIGHT SEASON: 00 ⚔️🛡️_*
+╰━━━━━━━━━━━━━❖
+*❏ Gain duel : 00 🧭*
+*❏ Gain quête 00 🧭:*
+*❏ Gain aventure : 00 🧭*
+*❏ Solde: 1000 🧭*
+
+     ━━━━━━━━━━━
+━━━━━━
+*Equipment🎒: 00🥋*`;
+            zk.sendMessage(dest, { image: { url: lien }, caption: msg }, { quoted: ms });
+   
+        }
+    }
+);
+
+
+france(
+    {
+        nomCom: 'Xiao.stat',
+        categorie: 'Maestria rpverse'
+    },
+    async (dest, zk, commandeOptions) => {
+        const { repondre, arg, ms } = commandeOptions;
+
+        if (!arg || arg.length === 0)  {
+            const lien = 'https://telegra.ph/file/9a1c19808b8b4e6bcf636.jpg';
+            const msg = `╭━━━「 𝚳𝚫𝚵𝐒𝚻𝚪𝚰𝚫 」❏
+┆ *_♯ROLE PLAYVERSE_*
+╰━━━━━━━━━━━━━❖
+*❖Pseudo*👤:「 XIAO WILLIAM」
+*❖Type de mage*👤:「 *_MAHOTSUKAI_*」
+*❖Race*:「 *_HUMAIN_*」
+*❖Rang*:「E 」
+*❖Classe*:「 *_NOVICE 🥉_*」
+        ━━━━━━━━━━━━╮
+                                                           ┆
+❖━━━━━━━━━━━━━╯
+*défaites: 00*
+*Nombre de victoires: 00*
+*_Trophee : 00🏆_*
+╭━━━「 𝚳𝚫𝚵𝐒𝚻𝚪𝚰𝚫 」❏
+┆ *_♯TOTAL FIGHT SEASON: 00 ⚔️🛡️_*
+╰━━━━━━━━━━━━━❖
+*❏ Gain duel : 00 🧭*
+*❏ Gain quête 00 🧭:*
+*❏ Gain aventure : 00 🧭*
+*❏ Solde: 1000 🧭*
+
+     ━━━━━━━━━━━
+━━━━━━
+*Equipment🎒: 00🥋*`;
+            zk.sendMessage(dest, { image: { url: lien }, caption: msg }, { quoted: ms });
+   
+        }
+    }
+);
+
+
+france(
+    {
+        nomCom: 'Yû.stat',
+        categorie: 'Maestria rpverse'
+    },
+    async (dest, zk, commandeOptions) => {
+        const { repondre, arg, ms } = commandeOptions;
+
+        if (!arg || arg.length === 0)  {
+            const lien = 'https://telegra.ph/file/9a1c19808b8b4e6bcf636.jpg';
+            const msg = `╭━━━「 𝚳𝚫𝚵𝐒𝚻𝚪𝚰𝚫 」❏
+┆ *_♯ROLE PLAYVERSE_*
+╰━━━━━━━━━━━━━❖
+*❖Pseudo*👤:「 YÛ BLAQ'S」
+*❖Type de mage*👤:「 *_MAHOTSUKAI_*」
+*❖Race*:「 *_HUMAIN_*」
+*❖Rang*:「E 」
+*❖Classe*:「 *_NOVICE 🥉_*」
+        ━━━━━━━━━━━━╮
+                                                           ┆
+❖━━━━━━━━━━━━━╯
+*défaites: 00*
+*Nombre de victoires: 00*
+*_Trophee : 00🏆_*
+╭━━━「 𝚳𝚫𝚵𝐒𝚻𝚪𝚰𝚫 」❏
+┆ *_♯TOTAL FIGHT SEASON: 00 ⚔️🛡️_*
+╰━━━━━━━━━━━━━❖
+*❏ Gain duel : 00 🧭*
+*❏ Gain quête 00 🧭:*
+*❏ Gain aventure : 00 🧭*
+*❏ Solde: 1000 🧭*
+
+     ━━━━━━━━━━━
+━━━━━━
+*Equipment🎒: 00🥋*`;
+            zk.sendMessage(dest, { image: { url: lien }, caption: msg }, { quoted: ms });
+   
+        }
+    }
+);
+
+
+france(
+    {
+        nomCom: 'Yu.stat',
+        categorie: 'Maestria rpverse'
+    },
+    async (dest, zk, commandeOptions) => {
+        const { repondre, arg, ms } = commandeOptions;
+
+        if (!arg || arg.length === 0)  {
+            const lien = 'https://telegra.ph/file/9a1c19808b8b4e6bcf636.jpg';
+            const msg = `╭━━━「 𝚳𝚫𝚵𝐒𝚻𝚪𝚰𝚫 」❏
+┆ *_♯ROLE PLAYVERSE_*
+╰━━━━━━━━━━━━━❖
+*❖Pseudo*👤:「 YÛ KUNN」
+*❖Type de mage*👤:「 *_MAHOTSUKAI_*」
+*❖Race*:「 *_HUMAIN_*」
+*❖Rang*:「E 」
+*❖Classe*:「 *_NOVICE 🥉_*」
+        ━━━━━━━━━━━━╮
+                                                           ┆
+❖━━━━━━━━━━━━━╯
+*défaites: 00*
+*Nombre de victoires: 00*
+*_Trophee : 00🏆_*
+╭━━━「 𝚳𝚫𝚵𝐒𝚻𝚪𝚰𝚫 」❏
+┆ *_♯TOTAL FIGHT SEASON: 00 ⚔️🛡️_*
+╰━━━━━━━━━━━━━❖
+*❏ Gain duel : 00 🧭*
+*❏ Gain quête 00 🧭:*
+*❏ Gain aventure : 00 🧭*
+*❏ Solde: 1000 🧭*
+
+     ━━━━━━━━━━━
+━━━━━━
+*Equipment🎒: 00🥋*`;
+            zk.sendMessage(dest, { image: { url: lien }, caption: msg }, { quoted: ms });
+   
+        }
+    }
+);
+
+
+france(
+    {
+        nomCom: 'Zenitsu.stat',
+        categorie: 'Maestria rpverse'
+    },
+    async (dest, zk, commandeOptions) => {
+        const { repondre, arg, ms } = commandeOptions;
+
+        if (!arg || arg.length === 0)  {
+            const lien = 'https://telegra.ph/file/9a1c19808b8b4e6bcf636.jpg';
+            const msg = `╭━━━「 𝚳𝚫𝚵𝐒𝚻𝚪𝚰𝚫 」❏
+┆ *_♯ROLE PLAYVERSE_*
+╰━━━━━━━━━━━━━❖
+*❖Pseudo*👤:「 ZENITSU」
+*❖Type de mage*👤:「 *_MAHOTSUKAI_*」
+*❖Race*:「 *_HUMAIN_*」
+*❖Rang*:「E 」
+*❖Classe*:「 *_NOVICE 🥉_*」
+        ━━━━━━━━━━━━╮
+                                                           ┆
+❖━━━━━━━━━━━━━╯
+*défaites: 00*
+*Nombre de victoires: 00*
+*_Trophee : 00🏆_*
+╭━━━「 𝚳𝚫𝚵𝐒𝚻𝚪𝚰𝚫 」❏
+┆ *_♯TOTAL FIGHT SEASON: 00 ⚔️🛡️_*
+╰━━━━━━━━━━━━━❖
+*❏ Gain duel : 00 🧭*
+*❏ Gain quête 00 🧭:*
+*❏ Gain aventure : 00 🧭*
+*❏ Solde: 1000 🧭*
+
+     ━━━━━━━━━━━
+━━━━━━
+*Equipment🎒: 00🥋*`;
+            zk.sendMessage(dest, { image: { url: lien }, caption: msg }, { quoted: ms });
+   
+        }
+    }
+);
+
+
+france(
+    {
+        nomCom: 'Toyomi.stat',
+        categorie: 'Maestria rpverse'
+    },
+    async (dest, zk, commandeOptions) => {
+        const { repondre, arg, ms } = commandeOptions;
+
+        if (!arg || arg.length === 0)  {
+            const lien = 'https://telegra.ph/file/9a1c19808b8b4e6bcf636.jpg';
+            const msg = `╭━━━「 𝚳𝚫𝚵𝐒𝚻𝚪𝚰𝚫 」❏
+┆ *_♯ROLE PLAYVERSE_*
+╰━━━━━━━━━━━━━❖
+*❖Pseudo*👤:「 TOYOMI ZIOLES」
+*❖Type de mage*👤:「 *_MAHOTSUKAI_*」
+*❖Race*:「 *_HUMAIN_*」
+*❖Rang*:「E 」
+*❖Classe*:「 *_NOVICE 🥉_*」
+        ━━━━━━━━━━━━╮
+                                                           ┆
+❖━━━━━━━━━━━━━╯
+*défaites: 00*
+*Nombre de victoires: 00*
+*_Trophee : 00🏆_*
+╭━━━「 𝚳𝚫𝚵𝐒𝚻𝚪𝚰𝚫 」❏
+┆ *_♯TOTAL FIGHT SEASON: 00 ⚔️🛡️_*
+╰━━━━━━━━━━━━━❖
+*❏ Gain duel : 00 🧭*
+*❏ Gain quête 00 🧭:*
+*❏ Gain aventure : 00 🧭*
+*❏ Solde: 1000 🧭*
+
+     ━━━━━━━━━━━
+━━━━━━
+*Equipment🎒: 00🥋*`;
+            zk.sendMessage(dest, { image: { url: lien }, caption: msg }, { quoted: ms });
+   
+        }
+    }
+);
+
+
+france(
+    {
+        nomCom: 'Mei.stat',
+        categorie: 'Maestria rpverse'
+    },
+    async (dest, zk, commandeOptions) => {
+        const { repondre, arg, ms } = commandeOptions;
+
+        if (!arg || arg.length === 0)  {
+            const lien = 'https://telegra.ph/file/9a1c19808b8b4e6bcf636.jpg';
+            const msg = `╭━━━「 𝚳𝚫𝚵𝐒𝚻𝚪𝚰𝚫 」❏
+┆ *_♯ROLE PLAYVERSE_*
+╰━━━━━━━━━━━━━❖
+*❖Pseudo*👤:「 MEI YAO」
+*❖Type de mage*👤:「 *_MAHOTSUKAI_*」
+*❖Race*:「 *_HUMAIN_*」
+*❖Rang*:「E 」
+*❖Classe*:「 *_NOVICE 🥉_*」
+        ━━━━━━━━━━━━╮
+                                                           ┆
+❖━━━━━━━━━━━━━╯
+*défaites: 00*
+*Nombre de victoires: 00*
+*_Trophee : 00🏆_*
+╭━━━「 𝚳𝚫𝚵𝐒𝚻𝚪𝚰𝚫 」❏
+┆ *_♯TOTAL FIGHT SEASON: 00 ⚔️🛡️_*
+╰━━━━━━━━━━━━━❖
+*❏ Gain duel : 00 🧭*
+*❏ Gain quête 00 🧭:*
+*❏ Gain aventure : 00 🧭*
+*❏ Solde: 1000 🧭*
+
+     ━━━━━━━━━━━
+━━━━━━
+*Equipment🎒: 00🥋*`;
+            zk.sendMessage(dest, { image: { url: lien }, caption: msg }, { quoted: ms });
+   
+        }
+    }
+);
+
+
+france(
+    {
+        nomCom: 'Hajime.stat',
+        categorie: 'Maestria rpverse'
+    },
+    async (dest, zk, commandeOptions) => {
+        const { repondre, arg, ms } = commandeOptions;
+
+        if (!arg || arg.length === 0)  {
+            const lien = 'https://telegra.ph/file/9a1c19808b8b4e6bcf636.jpg';
+            const msg = `╭━━━「 𝚳𝚫𝚵𝐒𝚻𝚪𝚰𝚫 」❏
+┆ *_♯ROLE PLAYVERSE_*
+╰━━━━━━━━━━━━━❖
+*❖Pseudo*👤:「 HAJIME NEXUS」
+*❖Type de mage*👤:「 *_MAHOTSUKAI_*」
+*❖Race*:「 *_HUMAIN_*」
+*❖Rang*:「E 」
+*❖Classe*:「 *_NOVICE 🥉_*」
+        ━━━━━━━━━━━━╮
+                                                           ┆
+❖━━━━━━━━━━━━━╯
+*défaites: 00*
+*Nombre de victoires: 00*
+*_Trophee : 00🏆_*
+╭━━━「 𝚳𝚫𝚵𝐒𝚻𝚪𝚰𝚫 」❏
+┆ *_♯TOTAL FIGHT SEASON: 00 ⚔️🛡️_*
+╰━━━━━━━━━━━━━❖
+*❏ Gain duel : 00 🧭*
+*❏ Gain quête 00 🧭:*
+*❏ Gain aventure : 00 🧭*
+*❏ Solde: 1000 🧭*
+
+     ━━━━━━━━━━━
+━━━━━━
+*Equipment🎒: 00🥋*`;
+            zk.sendMessage(dest, { image: { url: lien }, caption: msg }, { quoted: ms });
+   
+        }
+    }
+);
+
+
