89ba65ccdd52681c3e4b2215cafe6a560a951ead
Lord Eiser’s brick’s <168234265+XU3legacy@users.noreply.github.com>
2024-06-10 21:02:41 +0100

    Update box.js

--- commands/box.js
+++ commands/box.js
@@ -14,8 +14,30 @@ france(
             const msg = `Bonjour @${numeroUtilisateur.split("@")[0]}, tu as exécuté la commande '${commandeOptions.nomCom}'. Voici une image pour toi:`;
             zk.sendMessage(dest, { image: { url: lien }, caption: msg }, { quoted: ms });
 
-            // Ajoutez ici le code pour envoyer un message si aucun argument n'est fourni
-            const messageAEnvoyer = "Veuillez fournir un argument pour cette commande.";
+            // Ajoutez ici le code pour envoyer un message si aucun argument n'est fo╭━━━「 𝚳𝚫𝚵𝐒𝚻𝚪𝚰𝚫 」❏
+┆*♯ ROLE PLAY VERSE*
+╰━━━━━━━━━━━━━❖
+*❖Pseudo*👤:「 」
+*❖Type de mage*👤:「 」
+*❖Race*:「 」
+*❖Rang*:「 」
+*❖Classe*:「 」
+        ━━━━━━━━━━━━╮
+                                                           ┆
+❖━━━━━━━━━━━━━╯
+*Nombre de défaites*:
+*Nombre de victoires*:
+╭━━━「 𝚳𝚫𝚵𝐒𝚻𝚪𝚰𝚫 」❏
+┆*♯ TOTAL FIGHT SEASON*:
+╰━━━━━━━━━━━━━❖
+*❏ Gain duel*:
+*❏ Gain quête*:
+*❏ Gain aventure*:
+*❏ Solde*:
+
+     ━━━━━━━━━━━
+━━━━━━
+*Equipment🎒*:";
             zk.sendMessage(dest, { text: messageAEnvoyer }, { quoted: ms });
         }
     }
